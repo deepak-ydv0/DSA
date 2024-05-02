c4c2115857de0d1b420292ce59851c7817c0869d
Deepak Yadav <112889421+deepak-ydv0@users.noreply.github.com>
2024-05-02 05:12:53 +0000

    stack

--- 06_stack.js
+++ 06_stack.js
@@ -1,10 +1,47 @@
  let data = []
  
- let currntSize = data.length()
+ let currntSize = data.length
  let max = 5
 
  function push(newVal) {
-    if (condition) {
-        
+    if (currntSize<max) {
+      data[currntSize] = newVal
+      currntSize++
+    }else{
+      console.log(`stack is full ${newVal}`);
     }
+
  }
+
+ 
+
+
+function pop() {
+   if (currntSize>0) {
+      currntSize = currntSize-1;
+      data.length = currntSize
+   }else{
+      console.log("stack is empty");
+   }
+}
+
+ push(10)
+ push(20)
+ push(30)
+ push(40)
+ push(50)
+ push(60)
+ push(70)
+ push(80)
+ push(90)
+ push(100)
+ console.log(data);
+ console.log(currntSize);
+ pop()
+ pop()
+ pop()
+ pop()
+ pop()
+ pop()
+ 
+ console.log(data);
